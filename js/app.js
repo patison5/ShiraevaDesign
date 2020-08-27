32cd7a68d2d14013d9c7c022c71cc9d8aadfb057
patison5 <patison4@yandex.ru>
2020-08-27 16:24:08 +0300

    реализовал основу декстопной версии

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 window.onload = function() {
-	
+
 	var bxs = $(document).ready(function(){
 		$('.slider1').bxSlider({
 			auto: true,
 			pager: false,
 			autoControls: false,
 			slideMargin: 10,
+			touchEnabled:false,
+			pause: 50000
+		});
+	});
+
+	var bxs2 = $(document).ready(function(){
+		$('#portfolio_1').bxSlider({
+			auto: true,
+			pager: false,
+			autoControls: false,
 			touchEnabled:false,
 			pause: 50000
 		});
